faa5290e0e8d49535e912bc96c00100337a76358
Atryee Chatterjee <atryeechatterjee31@gmail.com>
2026-08-09 20:20:50 +0530

    refactor: document non-optimized power approach in Power.java

--- Recursion/Power.java
+++ Recursion/Power.java
@@ -20,6 +20,12 @@
 package Recursion;
 
 public class Power {
+    // public static int power(int a, int n) { // Not optimized , Time complexity: O(n)
+    //     if(n == 0){
+    //         return 1;
+    //     }
+    //     return a * power(a, n-1);
+    // }
     public static int power(int a, int n) {
         if (n == 0) {
             return 1;
