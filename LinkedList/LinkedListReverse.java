6b58d0d08eaa3be62d2e15c12f63f301b0c712e6
Atryee Chatterjee <atryeechatterjee31@gmail.com>
2026-09-10 22:48:38 +0530

    Added linked list reverse and nth node removal

---
+++ LinkedList/LinkedListReverse.java
@@ -0,0 +1,62 @@
+package LinkedList;
+
+public class LinkedListReverse {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void reverse(){
+        Node prev = null;
+        Node curr = tail = head;
+        Node next;
+
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        head = prev;
+    }
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head; 
+        head = newNode;
+    }
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+    public static void main(String[] args) {
+        LinkedListReverse ll = new LinkedListReverse();
+        ll.addFirst(3);
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.addFirst(0);
+        ll.print();
+
+        ll.reverse();
+        ll.print();
+    }
+    
+}
---
+++ LinkedList/RemoveNthNodeFromEnd.java
@@ -0,0 +1,78 @@
+package LinkedList;
+
+public class RemoveNthNodeFromEnd {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public void removeNthFromEnd(int n) {
+        // Calculate size
+        Node temp = head;
+        int sz = 0;
+
+        while (temp != null) {
+            temp = temp.next;
+            sz++;
+        }
+
+        if (n == sz) {
+            head = head.next;
+            return;
+        }
+
+        int i = 1;
+        int iToFind = sz - n;
+        Node prev = head;
+        while (i < iToFind) {
+            prev = prev.next;
+            i++;
+        }
+        prev.next = prev.next.next;
+        return;
+    }
+
+    public static void main(String[] args) {
+        RemoveNthNodeFromEnd ll = new RemoveNthNodeFromEnd();
+        ll.addFirst(5);
+        ll.addFirst(4);
+        ll.addFirst(3);
+        ll.addFirst(2);
+        ll.addFirst(1);
+        ll.print();
+
+        int n = 2; // Example: Remove the 2nd (4) node from the end
+        ll.removeNthFromEnd(n);
+        ll.print();
+    }
+
+}
