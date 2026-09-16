7bf17c2aed35c250eed5cb0f463c5bf3ca53492a
Atryee Chatterjee <atryeechatterjee31@gmail.com>
2026-09-16 19:46:26 +0530

    Added merge sort for linked list

---
+++ LinkedList/MergeSort.java
@@ -0,0 +1,131 @@
+/*
+Problem: Sort a Singly Linked List using Merge Sort
+
+Approach:
+- Find the middle node using slow and fast pointers
+- Split the linked list into left and right halves
+- Recursively sort both halves
+- Merge the two sorted halves using the merge() method
+- Return the merged sorted list
+
+Complexity:
+Time: O(n log n)
+Space: O(log n)
+
+Key Idea:
+- Divide the linked list into smaller halves
+- Recursively sort each half
+- Merge the sorted halves to get the final sorted list
+*/
+package LinkedList;
+
+public class MergeSort {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public Node merge(Node head1, Node head2) {
+        Node mergedLL = new Node(-1);
+        Node temp = mergedLL;
+
+        while (head1 != null && head2 != null) {
+            if (head1.data <= head2.data) {
+                temp.next = head1;
+                head1 = head1.next;
+                temp = temp.next;
+            } else {
+                temp.next = head2;
+                head2 = head2.next;
+                temp = temp.next;
+            }
+        }
+
+        while (head1 != null) {
+            temp.next = head1;
+            head1 = head1.next;
+            temp = temp.next;
+        }
+        while (head2 != null) {
+            temp.next = head2;
+            head2 = head2.next;
+            temp = temp.next;
+        }
+
+        // Merge
+        return mergedLL.next;
+    }
+
+    public Node getMid(Node head) {
+        Node slow = head;
+        Node fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    public Node mergeSort(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        // Find mid
+        Node mid = getMid(head);
+
+        // Left && Right MS
+        Node rightHead = mid.next;
+        mid.next = null;
+        Node newLeft = mergeSort(head);
+        Node newRight = mergeSort(rightHead);
+
+        return merge(newLeft, newRight);
+
+    }
+
+    public static void main(String[] args) {
+        MergeSort ll = new MergeSort();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.addFirst(3);
+        ll.addFirst(4);
+        ll.addFirst(5);
+        ll.print();
+
+        ll.head = ll.mergeSort(head);
+        ll.print();
+
+    }
+}
