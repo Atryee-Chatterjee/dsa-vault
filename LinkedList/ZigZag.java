297a879ac00016e842c414efb499ca45ec409498
Atryee Chatterjee <atryeechatterjee31@gmail.com>
2026-09-23 21:32:15 +0530

    Added solution for zig zag linkedlist

--- LinkedList/ZigZag.java
+++ LinkedList/ZigZag.java
@@ -1,3 +1,23 @@
+/*
+Problem: Convert a Singly Linked List into Zig-Zag Order
+
+Approach:
+- Find the middle of the linked list using slow and fast pointers
+- Split the list into two halves
+- Reverse the second half of the list
+- Alternately merge nodes from the first and reversed second half
+- Continue until all nodes are merged in zig-zag order
+
+Complexity:
+Time: O(n)
+Space: O(1)
+
+Key Idea:
+- Find the middle
+- Reverse the second half
+- Alternately merge both halfes
+- Example: 1->2->3->4->5->6 becomes 1->6->2->5->3->4
+*/
 package LinkedList;
 
 public class ZigZag {
@@ -12,8 +32,81 @@ public class ZigZag {
     }
     public static Node head;
     public static Node tail;
+    public static int size;
+
+    public void zigZag() {
+        // find mid
+        Node slow = head;
+        Node fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node mid = slow;
+
+        // Reverse 2nd half
+        Node curr = mid.next;
+        mid.next = null;
+        Node prev = null;
+        Node next;
+
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        Node right = prev;
+        Node left = head;
+        Node nextL, nextR;
+
+        // alt merge - zig zag merge
+        while (left != null && right != null) {
+            nextL = left.next;
+            left.next = right;
+            nextR = right.next;
+            right.next = nextL;
+
+            // update
+            left = nextL;
+            right = nextR;
+        }
+    }
+
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        size++;
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
 
     public static void main(String[] args) {
         ZigZag ll = new ZigZag();
+
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        ll.addLast(5);
+        ll.addLast(6);
+
+        ll.print();
+        ll.zigZag();
+        ll.print();
     }
 }
